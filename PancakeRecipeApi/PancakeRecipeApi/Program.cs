81af3e42e4ed00f4168bf3197b9422a1ff82072e
Caleb German <Caleb.German@Bentley.com>
2024-06-23 14:05:45 -0500

    Updated Cors

--- PancakeRecipeApi/PancakeRecipeApi/Program.cs
+++ PancakeRecipeApi/PancakeRecipeApi/Program.cs
@@ -2,32 +2,40 @@ namespace PancakeRecipeApi;
 
 public class Program
 {
-	public static void Main(string[] args)
-	{
-		var builder = WebApplication.CreateBuilder(args);
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-		// Add services to the container.
+        // Add services to the container.
 
-		builder.Services.AddControllers();
-		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-		builder.Services.AddEndpointsApiExplorer();
-		builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-		var app = builder.Build();
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAnyOrigin",
+                corsPolicyBuilder => corsPolicyBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+        });
 
-		// Configure the HTTP request pipeline.
-		if (app.Environment.IsDevelopment())
-		{
-			app.UseSwagger();
-			app.UseSwaggerUI();
-		}
+        var app = builder.Build();
 
-		app.UseHttpsRedirection();
+        app.UseCors("AllowAnyOrigin");
 
-		app.UseAuthorization();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
 
-		app.MapControllers();
+        app.UseHttpsRedirection();
 
-		app.Run();
-	}
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
